1aba9a318b8222d670a1d6fcbba49ff6e35bc3f9
w41k4z <alainricor@gmail.com>
2025-03-04 15:45:54 +0300

    Bootstrap added for vue js

--- app/javascript/components/ClientComponent.vue
+++ app/javascript/components/ClientComponent.vue
@@ -7,7 +7,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/categories">Categories</router-link>
     </nav>
-    
+
     <router-view></router-view>
   </div>
 </template>
--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import ClientComponent from '@/components/ClientComponent.vue';
 import router from '@/router/index.js'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     const app = createApp(ClientComponent);
     app.use(router)
