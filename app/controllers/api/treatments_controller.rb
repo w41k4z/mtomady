ccee2576e1c1dc84123a53347786dc2a93a41196
w41k4z <alainricor@gmail.com>
2025-03-04 22:27:15 +0300

    Treatment selection validation

--- app/controllers/api/treatments_controller.rb
+++ app/controllers/api/treatments_controller.rb
@@ -1,7 +1,25 @@
 class Api::TreatmentsController < ApplicationController
   def index
-    lang = params[:lang] || "en"
-    @treatments = TreatmentView.where(lang: lang)
+    lang = params[:lang]&.split('-')&.first || "en"
+    category_id = params[:category_id]
+    
+    query = TreatmentView.where("lang ILIKE ?", "#{lang}%")
+    query = query.where(category_id: category_id) if category_id.present?
+
+    @treatments = query
     render json: @treatments
+  end
+
+  def show
+    lang = params[:lang]&.split('-')&.first || "en"
+    id = params[:id]
+
+    treatment = TreatmentView.find_by(treatment_id: id, lang: lang)
+    
+    if treatment
+      render json: treatment
+    else
+      render json: { error: "Treatment not found" }, status: :not_found
+    end
   end
 end
